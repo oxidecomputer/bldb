83b8b4f9c182efe1cc2b99bed9677b87cf2fbee6
Dan Cross <cross@oxidecomputer.com>
2025-04-11 01:25:10 +0000

    cpio: mount a cpio miniroot as a ramdisk
    Support mounting a cpio archive as a ramdisk, in lieu of UFS.
    The usual file commands work here, albeit without symlink
    support.  Regardless, this is good enough to load a kernel and
    get it running, using the same commands and syntax as for UFS.
    
    Signed-off-by: Dan Cross <cross@oxidecomputer.com>

---
+++ src/io.rs
@@ -0,0 +1,59 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+use crate::result::Result;
+
+#[derive(Debug)]
+pub(crate) struct Sd {
+    pub(crate) ptr: *const u8,
+    pub(crate) len: usize,
+}
+
+impl Sd {
+    pub(crate) fn new(ptr: *const u8, len: usize) -> Sd {
+        Sd { ptr, len }
+    }
+
+    pub(crate) fn from_slice(bs: &[u8]) -> Sd {
+        Sd::new(bs.as_ptr(), bs.len())
+    }
+
+    pub(crate) fn as_ptr(&self) -> *const u8 {
+        self.ptr
+    }
+
+    pub(crate) fn len(&self) -> usize {
+        self.len
+    }
+
+    pub(crate) fn subset(&self, offset: usize, len: usize) -> Sd {
+        assert!(offset + len <= self.len);
+        let ptr = self.ptr.wrapping_add(offset);
+        Sd { ptr, len }
+    }
+}
+
+pub(crate) trait Read {
+    fn read(&self, off: u64, dst: &mut [u8]) -> Result<usize>;
+    fn size(&self) -> usize;
+}
+
+impl Read for &[u8] {
+    fn read(&self, off: u64, dst: &mut [u8]) -> Result<usize> {
+        let off = off as usize;
+        if off > self.len() {
+            return Ok(0);
+        }
+        let bytes = &self[off..];
+        let len = usize::min(bytes.len(), dst.len());
+        if len > 0 {
+            dst[..len].copy_from_slice(&bytes[..len]);
+        }
+        Ok(len)
+    }
+
+    fn size(&self) -> usize {
+        self.len()
+    }
+}
